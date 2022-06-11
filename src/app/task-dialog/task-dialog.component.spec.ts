b52e7d8f0b696154e739d5212f015c81720a9d49
pokemog <8730056+pokemog@users.noreply.github.com>
2022-06-11 12:15:52 -0700

    Fixing unit tests

--- src/app/task-dialog/task-dialog.component.spec.ts
+++ src/app/task-dialog/task-dialog.component.spec.ts
@@ -1,21 +1,48 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { TaskDialogComponent } from './task-dialog.component';
+import { FormsModule } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Task } from '../task/task';
+import { TaskDialogComponent, TaskDialogData } from './task-dialog.component';
 
 describe('TaskDialogComponent', () => {
   let component: TaskDialogComponent;
   let fixture: ComponentFixture<TaskDialogComponent>;
+  let mockTask: Task = {
+    title: "Test Title",
+    description: "Test Description"
+  }
+  let mockTaskDialogData: TaskDialogData = {
+    task: mockTask,
+    enableDelete: true
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TaskDialogComponent ]
-    })
-    .compileComponents();
+      imports: [
+        MatInputModule,
+        MatDialogModule,
+        FormsModule,
+        BrowserAnimationsModule
+      ],
+      declarations: [TaskDialogComponent],
+      providers: [
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {}
+        },
+        {
+          provide: MatDialogRef,
+          useValue: {}
+        }]
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TaskDialogComponent);
     component = fixture.componentInstance;
+    component.data = mockTaskDialogData;
     fixture.detectChanges();
   });
 
